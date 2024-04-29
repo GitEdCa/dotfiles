1e3e097e473f9ff07f027821ce52980326a21c26
elar <elar@seneca.com>
2024-04-28 21:35:56 -0500

    Replace emacs good base config

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,101 +1,145 @@
-;; This is only needed once, near the top of the file
-(eval-when-compile
-  ;; Following line is not needed if use-package.el is in ~/.emacs.d
-  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/")
-	       (require 'use-package)))
-;; bootstrap use-package
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
+(setq backup-directory-alist
+      `(("." . "~/.emacs.d/backups"))
+      auto-save-file-name-transforms
+      `((".*" "~/.emacs.d/auto-save-list/" t)))
 
-;expand-region
-(use-package expand-region
-  :ensure t  
-  :bind ("C-=" . er/expand-region))
+(setq custom-file "~/.emacs.d/custom.el")
+;; Prevent undo tree files from polluting your git repo
+(setq undo-tree-history-directory-alist '(("." . "~/.emacs.d/undo")))
 
+(add-to-list 'load-path "~/.emacs.d/better-defaults")
+(require 'better-defaults)
+
+;; Startup
+(setq inhibit-startup-screen t)
+(setq initial-scratch-message
+      ";; Hello world.\n")
+
+;; Zoom
+(set-face-attribute 'default nil :height 120)
+
+;; Save History
+(savehist-mode +1)
+(setq savehist-additional-variables '(kill-ring search-ring regexp-search-ring))
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+;; evil mode
+(use-package evil
+  :ensure t
+  :config
+  (evil-mode t)
+  (setq evil-disable-insert-state-bindings 't))
+
+(use-package undo-tree
+  :ensure t
+  :after evil
+  :diminish
+  :config
+  (evil-set-undo-system 'undo-tree)
+  (global-undo-tree-mode 1))
+
+;; magit
+(use-package magit
+  :ensure t)
+
+;; multiple cursors
 (use-package multiple-cursors
   :ensure t
-  :bind ("C-c m c" . mc/edit-lines)
-  ("C->" . mc/mark-next-like-this)
-  ("C-<" . mc/mark-previous-like-this)
-  ("C-c C-<" . mc/mark-all-like-this))
+  :config
+  (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+  (global-set-key (kbd "C->")         'mc/mark-next-like-this)
+  (global-set-key (kbd "C-<")         'mc/mark-previous-like-this)
+  (global-set-key (kbd "C-c C-<")     'mc/mark-all-like-this)
+  (global-set-key (kbd "C-\"")        'mc/skip-to-next-like-this)
+  (global-set-key (kbd "C-:")         'mc/skip-to-previous-like-this))
 
-;set theme
-(load-theme 'deeper-blue)
+;; move-text
+(use-package move-text
+  :ensure t
+  :config
+  (global-set-key (kbd "M-p") 'move-text-up)
+  (global-set-key (kbd "M-n") 'move-text-down))
 
-(setq compilation-directory-locked nil)
-(scroll-bar-mode -1)
-(setq shift-select-mode nil)
-(setq enable-local-variables nil)
+;; company
+(use-package company
+  :ensure t
+  :hook (emacs-lisp-mode . company-tng-mode)
+  :config
+  (setq company-idle-delay 0.5
+	company-minimum-prefix-length 2))
 
-(display-battery-mode 1)
+(use-package eglot
+  :ensure t
+  :demand t
+  :bind (:map eglot-mode-map
+	      ("<f6>" . eglot-format-buffer)
+	      ("C-c a" . eglot-code-actions)
+	      ("C-c d" . eldoc)
+	      ("C-c r" . eglot-rename))
+  :config
+  (setq eglot-ignored-server-capabilities '(:inlayHintProvider)))
 
-; avoid welcome page
-(setq inhibit-startup-screen t)
-; Turn off the toolbar
-(tool-bar-mode 0)
+(use-package c-ts-mode
+  :ensure t
+  :after (eglot)
+  :hook ((c-ts-mode . eglot-ensure)
+	 (c-ts-mode . company-tng-mode)
+	 (c-ts-mode . (lambda ()
+			(eglot-inlay-hints-mode -1))))
+  :config
+  (add-to-list 'eglot-server-programs '(c-ts-mode . ("clangd"
+						     "--header-insertion=never"))))
 
-; activate ido mode
-(setq ido-enable-flex-matching t)
-(setq ido-everywhere t)
-(ido-mode 1)
+(add-to-list 'major-mode-remap-alist '(c-mode . c-ts-mode))
 
-; moving to other window
-(global-set-key (kbd "C-;")  #'other-window)
+;; disable all bars
+;;(tool-bar-mode -1)
+;;(menu-bar-mode -1)
+;;(scroll-bar-mode -1)
 
-; save buffers
-(define-key global-map "\es" 'save-buffer)
+;; pretend like I have winum
+;;(global-set-key (kbd "M-1") 'other-window)
+;;(global-set-key (kbd "M-2") 'other-window)
+(setq gdb-many-windows t) ; set gdb-many-windows on gdb automatically
 
-; Move windows with meta
-(setq ediff-split-window-function 'split-window-vertically)
+(use-package expand-region
+  :ensure t
+  :bind (("C-=" . er/expand-region)
+	 ("C--" . er/contract-region)))
 
-; Turn off the bell on Mac OS X
-(defun nil-bell ())
-(setq ring-bell-function 'nil-bell)
+(use-package vertico
+  :ensure t
+  :custom
+  (vertico-count 13)                    ; Number of candidates to display
+  (vertico-resize t)
+  (vertico-cycle nil)
+  :config
+  (vertico-mode))
 
-;; Org mode
-(use-package org
-	     :ensure t)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(multiple-cursors expand-region ace-jump-mode org use-package helm-projectile general fzf evil-leader dumb-jump doom-themes)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(defun my-compile ()
-  (interactive)
-  ;; Switch to `*shell*'
-  (shell)
-  ;; Goto last prompt, clear old input if any, and insert new one
-  (goto-char (point-max))
-  (comint-kill-input)
-  (insert "build")
-  ;; Execute
-  (comint-send-input))
+(use-package hotfuzz
+  :ensure t
+  :init
+  (setq completion-styles '(hotfuzz)))
 
-(global-set-key (kbd "M-m") 'my-compile)
+;; UTF-8
+(set-language-environment "UTF-8")
+(set-default-coding-systems 'utf-8)
+(set-keyboard-coding-system 'utf-8-unix)
+(set-terminal-coding-system 'utf-8-unix)
 
-(defun eshell-execute-selection ()
-  "Insert text of current selection or clipboard in eshell and execute."
-  (interactive)
-  (require 'eshell)
-  (let ((command (or (buffer-substring (mark) (point))
-         x-last-selected-text-clipboard)))
-    (let ((buf (current-buffer)))
-      (unless (get-buffer eshell-buffer-name)
-        (eshell))
-      (display-buffer eshell-buffer-name t)
-      (switch-to-buffer-other-window eshell-buffer-name)
-      (end-of-buffer)
-      (eshell-kill-input)
-      (insert command)
-      (eshell-send-input)
-      (end-of-buffer)
-      (switch-to-buffer-other-window buf))))
+(use-package nord-theme
+  :ensure t
+  :config
+  (load-theme 'nord t))
+
+
+(use-package rainbow-mode
+  :ensure t)
+
+;; dired
