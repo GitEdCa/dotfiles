36572e36007d78b0c350c9708ee378dc3a3492c1
elar <elar@seneca.com>
2024-04-30 21:00:39 -0500

    Add dap plugin neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -540,6 +540,56 @@ require('lazy').setup({
       vim.keymap.set({'n', 't'}, "<C-k>", function() harpoon:list():select(2) end)
       vim.keymap.set({'n', 't'}, "<C-l>", function() harpoon:list():select(3) end)
     end,
+  },
+
+  {
+    "rcarriga/nvim-dap-ui",
+	dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"},
+    opts = {},
+    config = function(_, opts)
+      require("dapui").setup()
+      local dap, dapui = require("dap"), require("dapui")
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+
+      dap.adapters.gdb = {
+        type = "executable",
+        command = "gdb",
+        args = { "-i", "dap" }
+      }
+      dap.configurations.c = {
+        {
+          name = "Launch",
+          type = "gdb",
+          request = "launch",
+          program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+          end,
+          cwd = "${workspaceFolder}",
+          stopAtBeginningOfMainSubprogram = false,
+        },
+      }
+
+      vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
+      vim.keymap.set('n', '<S-F5>', function() require('dap').terminate() end)
+      vim.keymap.set('n', '<F6>', function() require('dap').step_over() end)
+      vim.keymap.set('n', '<F7>', function() require('dap').step_into() end)
+      vim.keymap.set('n', '<S-F7>', function() require('dap').step_out() end)
+      vim.keymap.set('n', '<F8>', function() require('dap').toggle_breakpoint() end)
+      vim.keymap.set('n', '<S-F8>', function() require('dap').repl.open() end)
+      vim.keymap.set('n', '<S-F6>', function() require('dap').run_last() end)
+
+    end,
   }
 }, {
   ui = {
